63a29c4f1fef067a18412aea938a55fe92e6d351
Justin Collins <Cinnabonmon1@gmail.com>
2026-03-18 22:49:15 -0500

    Fix image change for buttons

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ document.addEventListener("click", (e) => {
     const answer = e.target.parentElement.nextElementSibling;
     answer.classList.toggle("open");
   }
-});
+  if(e.target.getAttribute('src') === './assets/images/icon-plus.svg'){
+    e.target.src = './assets/images/icon-minus.svg'
+  } else {
+    e.target.src = './assets/images/icon-plus.svg'
+  }
+}); 
